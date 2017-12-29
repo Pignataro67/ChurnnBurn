e38131371e483af5e703a233ab2fe69743805d3a
Pat  Pignataro <pat6284@msn.com>
2017-12-29 20:42:25 +0000

    adding a get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,29 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect '/workouts'
     else
-      flash[:message] = @user.errors.full_messages.join(',')
+      flash[:message] = @user.errors.full_messages.join(', ')
       redirect to '/signup'
     end
   end
+
+  get '/login' do
+    if !session[:user_id]
+      erb :'users/login'
+    else
+      redirect '/workouts'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/workouts'
+    else
+      flash[:message] = "Sorry, that Username/password does not match any of our records!"
+      redirect to '/signup'
+    end
+  end
+
+
 end
