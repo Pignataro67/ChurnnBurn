456d6502487471dbb4243277964f9ec4d26b1206
Pat  Pignataro <pat6284@msn.com>
2018-01-02 21:02:26 +0000

    adding get workouts new, id and post workout id

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -6,6 +6,11 @@ class WorkoutsController < ApplicationController
     erb :'workouts/workouts'
   end
 
+  get '/workouts/new' do
+    authenticate_user
+    erb :'/workouts/create_workout'
+  end
+
   post '/workouts' do
     authenticate_user
     @workout = current_user.workouts.build(name: params[:name], back: params[:back], legs: params[:legs], chest: params[:chest], shoulders: params[:shoulders], triceps: params[:triceps], biceps: params[:biceps], core: params[:core])
@@ -17,4 +22,24 @@ class WorkoutsController < ApplicationController
       redirect to '/workouts/new'
     end
   end
+
+  get '/workouts/:id' do
+    authenticate_user
+    @workout = Workout.find(params[:id])
+    erb :'/workouts/show_workout'
+  end
+
+  post '/workouts/:id' do
+    authenticate_user
+    @workout = Workout.find(params[:id])
+    if params[:name] == ""
+      flash[:workout_name_at_update] = "Your workout must have a name!"
+      redirect to 'workouts/#{@workout.id}/edit'
+    end
+    @workout.update(name: params[:name], back: params[:back], legs: params[:legs], chest: params[:chest], shoulders: params[:shoulders], triceps: params[:triceps], biceps: params[:biceps], core: params[:core])
+    flash[:workout_updated] = "Your workout has been updated!"
+    redirect to "users/#{current_user.id}"
+  end
+
+
 end
