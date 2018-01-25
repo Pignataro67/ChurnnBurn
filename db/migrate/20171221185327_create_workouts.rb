4c76d2c703346b189be9a27a4a18c5c031aebb1e
Pat  Pignataro <pat6284@msn.com>
2018-01-25 21:10:03 +0000

    adding hamstring and finisher exercise options to app

--- db/migrate/20171221185327_create_workouts.rb
+++ db/migrate/20171221185327_create_workouts.rb
@@ -3,6 +3,7 @@ class CreateWorkouts < ActiveRecord::Migration
   def change
     create_table :workouts do |t|
       t.string :back
+      t.string :quads
       t.string :legs
       t.string :chest
       t.string :shoulders
@@ -10,6 +11,10 @@ class CreateWorkouts < ActiveRecord::Migration
       t.string :biceps
       t.string :core
       t.integer :user_id
+      t.string :name
+      t.string :hamstrings
+      t.string :namee
+      t.string :finisher
     end
   end
 end
