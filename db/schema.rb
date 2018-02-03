f0880e01a8550ca10404f7ec517d59e1aa3fbbfa
Pat  Pignataro <pat6284@msn.com>
2018-02-03 00:31:36 +0000

    updating schema with removal of workout_id from users

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -14,10 +15,9 @@
 ActiveRecord::Schema.define(version: 20171221185327) do
 
   create_table "users", force: :cascade do |t|
-    t.string  "username"
-    t.string  "email"
-    t.string  "password_digest"
-    t.integer "workout_id"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
   end
 
   create_table "workouts", force: :cascade do |t|
